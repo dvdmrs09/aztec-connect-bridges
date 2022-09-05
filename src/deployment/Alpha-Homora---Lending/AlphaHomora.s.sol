7ab9fb3f6af8356276684e8b2733288f5031ae14
dvdmrs <dvdmrs09@gmail.com>
2022-09-05 11:15:03 -0700

    Update AlphaHomora.s.sol

--- src/deployment/Alpha-Homora---Lending/AlphaHomora.s.sol
+++ src/deployment/Alpha-Homora---Lending/AlphaHomora.s.sol
@@ -3,16 +3,16 @@
 pragma solidity >=0.8.4;
 
 import {BaseDeployment} from "../base/BaseDeployment.s.sol";
-import {ExampleBridgeContract} from "../../bridges/example/ExampleBridge.sol";
+import {AlphaHomorabridge1} from "../../bridges/Alpha-Homora---Lending/AlphaHomoraV2-Deposit.sol";
 
-contract ExampleDeployment is BaseDeployment {
+contract AlphaHomorabridge1Deployment is BaseDeployment {
     function deploy() public returns (address) {
-        emit log("Deploying example bridge");
+        emit log("Deploying AlphaHomorabridge1 bridge");
 
         vm.broadcast();
-        ExampleBridgeContract bridge = new ExampleBridgeContract(ROLLUP_PROCESSOR);
+        AlphaHomorabridge1 bridge = new AlphaHomorabridge1(ROLLUP_PROCESSOR);
 
-        emit log_named_address("Example bridge deployed to", address(bridge));
+        emit log_named_address("AlphaHomorabridge1 bridge deployed to", address(bridge));
 
         return address(bridge);
     }
@@ -20,6 +20,6 @@ contract ExampleDeployment is BaseDeployment {
     function deployAndList() public {
         address bridge = deploy();
         uint256 addressId = listBridge(bridge, 250000);
-        emit log_named_uint("Example bridge address id", addressId);
+        emit log_named_uint("AlphaHomorabridge1 bridge address id", addressId);
     }
 }

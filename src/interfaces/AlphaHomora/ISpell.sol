0ec68d49ab1b27b992d630d9658758c404a1fa17
dvdmrs <dvdmrs09@gmail.com>
2022-09-07 06:43:35 -0700

    Create ISpell.sol

---
+++ src/interfaces/AlphaHomora/ISpell.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.8.10;
+
+interface ISpell {
+    event AcceptGovernor(address governor);
+    event SetGovernor(address governor);
+    event SetPendingGovernor(address pendingGovernor);
+
+    struct Amounts {
+        uint256 a;
+        uint256 b;
+        uint256 c;
+        uint256 d;
+        uint256 e;
+        uint256 f;
+        uint256 g;
+        uint256 h;
+    }
+
+    struct RepayAmounts {
+        uint256 a;
+        uint256 b;
+        uint256 c;
+        uint256 d;
+        uint256 e;
+        uint256 f;
+        uint256 g;
+    }
+
+    function acceptGovernor() external;
+    function addLiquidityWERC20(address tokenA, address tokenB, Amounts memory amt) external payable;
+    function addLiquidityWStakingRewards(address tokenA, address tokenB, Amounts memory amt, address wstaking)
+        external
+        payable;
+    function approved(address, address) external view returns (bool);
+    function bank() external view returns (address);
+    function factory() external view returns (address);
+    function getAndApprovePair(address tokenA, address tokenB) external returns (address);
+    function governor() external view returns (address);
+    function harvestWStakingRewards(address wstaking) external;
+    function onERC1155BatchReceived(address, address, uint256[] memory, uint256[] memory, bytes memory)
+        external
+        returns (bytes4);
+    function onERC1155Received(address, address, uint256, uint256, bytes memory) external returns (bytes4);
+    function pairs(address, address) external view returns (address);
+    function pendingGovernor() external view returns (address);
+    function removeLiquidityWERC20(address tokenA, address tokenB, RepayAmounts memory amt) external;
+    function removeLiquidityWStakingRewards(address tokenA, address tokenB, RepayAmounts memory amt, address wstaking)
+        external;
+    function router() external view returns (address);
+    function setPendingGovernor(address _pendingGovernor) external;
+    function setWhitelistLPTokens(address[] memory lpTokens, bool[] memory statuses) external;
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+    function werc20() external view returns (address);
+    function weth() external view returns (address);
+    function whitelistedLpTokens(address) external view returns (bool);
+}
